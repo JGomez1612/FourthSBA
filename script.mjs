a55502223203f4895bc93d16af6484c9ca716457
JGomez1612 <jostgomez1612@gmail.com>
2025-08-27 16:54:28 -0400

    2nd Commit. Added iife to fetch data on load. Updated 2nd fetch function to be base for the search feature ill be adding.

--- script.mjs
+++ script.mjs
@@ -1,9 +1,22 @@
-async function fetchData(){
+
+
+(async function initialLoad(){
     try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon/meowscarada");
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1010");
+        const data = await res.json();
+        // console.log(data);
+        console.log(data.results[0]);
+    } catch (error) {
+        console.error(error)
+    }
+})();
+
+async function fetchPokemonByName(name){
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
 
         if(!res.ok){
-            throw new Error("Could not fetch resource");
+            throw new Error(`Pokemon ${name} not found`);
         }
         const data = await res.json();
         console.log(data);
@@ -13,4 +26,4 @@ async function fetchData(){
     }  
 }
 
-fetchData();
+fetchPokemonByName("Meowscarada");
